402062881871d2c0b946f5e83d288c063dde24da
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2024-04-09 22:07:25 +0200

    Add files via upload

---
+++ caf-practice/Naspi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caf_practice
+{
+    internal class Naspi
+    {
+        String nominativo;
+        double ultimaBustaPaga;
+        int mesiOccupazione;
+
+        public Naspi(String nominativo, double ultimaBustaPaga, int mesiOccupatione)
+        {
+            this.nominativo = nominativo;
+            this.ultimaBustaPaga = ultimaBustaPaga;
+            this.mesiOccupazione = mesiOccupatione;
+        }
+
+        public bool CheckNaspi()
+        {
+            double sussidio;
+
+            if (mesiOccupazione >= 24)
+            {
+                sussidio = ultimaBustaPaga * 0.7;
+                sussidio = CheckSussidio(sussidio);
+                Console.WriteLine("Puoi beneficiare della NASPI \n" +
+                    "Hai diritto a " + sussidio + " euro mensili");
+                return true;
+            }
+            else if (mesiOccupazione >= 12 && mesiOccupazione < 24)
+            {
+                sussidio = ultimaBustaPaga * 0.4;
+                sussidio = CheckSussidio(sussidio);
+                Console.WriteLine("Puoi beneficiare della NASPI \n" +
+                    "Hai diritto a " + sussidio + " euro mensili");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Non hai abbastanza mesi di occupazione \n" +
+                    "Non puoi beneficiare della NASPI");
+                return false;
+            }
+        }
+
+        public double CheckSussidio(double sussidio)
+        {
+            if (sussidio > 1200)
+                return 1200;
+            return sussidio;
+        }
+    }
+}
---
+++ caf-practice/Pensione.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caf_practice
+{
+    internal class Pensione
+    {
+        String nominativo;
+        int eta;
+        int anniLavorativi;
+
+        public Pensione(string nominativo, int eta, int anniLavorativi)
+        {
+            this.nominativo = nominativo;
+            this.eta = eta;
+            this.anniLavorativi = anniLavorativi;
+        }
+
+        public bool CheckPensione()
+        {
+            if ((eta + anniLavorativi) >= 100)
+            {
+                Console.WriteLine("Hai diritto alla pensione");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Non hai diritto alla pensione \n" +
+                    "Ti mancano " + (100 - (eta + anniLavorativi)) + " anni e/o anni lavorativi");
+                return false;
+            }
+        }
+    }
+}
